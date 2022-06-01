febda34bc4894a18727fd28631bfa5ff5c07804f
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-06-01 12:11:10 +0800

    Initial Commit

---
+++ Kosar/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Kosar
+//
+//  Created by ByteDance on 1/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Kosar/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Kosar
+//
+//  Created by ByteDance on 1/6/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
