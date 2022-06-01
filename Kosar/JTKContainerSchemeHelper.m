8c09877447697f3cfba80bfab03df1b5d97d2625
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-06-01 20:54:24 +0800

    Add helper class for Material Design container scheme

---
+++ Kosar/JTKContainerSchemeHelper.m
@@ -0,0 +1,23 @@
+//
+//  JTKContainerSchemeHelper.m
+//  Kosar
+//
+//  Created by ByteDance on 1/6/22.
+//
+
+#import "JTKContainerSchemeHelper.h"
+
+@implementation JTKContainerSchemeHelper
+
+MDCContainerScheme *containerScheme;
+
++ (MDCContainerScheme *)getContainerScheme {
+    if (!containerScheme) {
+        containerScheme = [[MDCContainerScheme alloc] init];
+        containerScheme.colorScheme.primaryColor = UIColor.blueColor;
+    }
+    return containerScheme;
+}
+
+
+@end
