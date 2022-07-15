028f98c74efb4010831ac122727ab625929b29fd
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-15 23:35:40 +0800

    Add API service class

---
+++ Kosar/JTKApiService.h
@@ -0,0 +1,14 @@
+//
+//  JTKApiService.h
+//  Kosar
+//
+//  Created by ByteDance on 15/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTKApiService : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
