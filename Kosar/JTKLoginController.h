80df5156c2fd550f303af4aa064ae4d6f55a5082
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-23 22:59:30 +0800

    Add login view controller

---
+++ Kosar/JTKLoginController.h
@@ -0,0 +1,12 @@
+//
+//  JTKLoginController.h
+//  Kosar
+//
+//  Created by ByteDance on 23/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTKLoginController : UIViewController
+
+@end
---
+++ Kosar/JTKLoginController.m
@@ -0,0 +1,21 @@
+//
+//  JTKLoginController.m
+//  Kosar
+//
+//  Created by ByteDance on 23/7/22.
+//
+
+#import "JTKLoginController.h"
+
+@interface JTKLoginController ()
+
+@end
+
+@implementation JTKLoginController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
