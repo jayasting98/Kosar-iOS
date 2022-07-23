1e51ee75acb2cfed2e0215c3ce48121e3c2a79db
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-23 23:22:07 +0800

    Add view model for login view controller

---
+++ Kosar/JTKLoginViewModel.h
@@ -0,0 +1,12 @@
+//
+//  JTKLoginViewModel.h
+//  Kosar
+//
+//  Created by ByteDance on 23/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTKLoginViewModel : NSObject
+
+@end
---
+++ Kosar/JTKLoginViewModel.m
@@ -0,0 +1,16 @@
+//
+//  JTKLoginViewModel.m
+//  Kosar
+//
+//  Created by ByteDance on 23/7/22.
+//
+
+#import "JTKLoginViewModel.h"
+
+@interface JTKLoginViewModel ()
+
+@end
+
+@implementation JTKLoginViewModel
+
+@end
