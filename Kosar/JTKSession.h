e599a76fcb98e3782478fbc2b14258d9e48a60a6
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-24 14:58:43 +0800

    Add sessions

---
+++ Kosar/JTKSession.h
@@ -0,0 +1,14 @@
+//
+//  JTKSession.h
+//  Kosar
+//
+//  Created by ByteDance on 24/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTKSession : NSObject
+
+@property (nonatomic) NSString *authenticationToken;
+
+@end
