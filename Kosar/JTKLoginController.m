a2399ee5378ca74b1fe0feeedab0fd1929f02f91
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-24 11:37:38 +0800

    Add password text field

--- Kosar/JTKLoginController.m
+++ Kosar/JTKLoginController.m
@@ -11,12 +11,15 @@
 #import "MaterialTextControls+OutlinedTextFields.h"
 
 static CGFloat const kMargin = 16;
+static CGFloat const kYGutter = 16;
 
 static NSString * const kUsernameTextFieldLabelText = @"Username";
+static NSString * const kPasswordTextFieldLabelText = @"Password";
 
 @interface JTKLoginController ()
 
 @property (nonatomic) MDCOutlinedTextField *usernameTextField;
+@property (nonatomic) MDCOutlinedTextField *passwordTextField;
 
 @end
 
@@ -26,12 +29,14 @@ static NSString * const kUsernameTextFieldLabelText = @"Username";
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.whiteColor;
     [self buildUsernameTextField];
+    [self buildPasswordTextField];
 }
 
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     [self layoutUsernameTextField];
+    [self layoutPasswordTextField];
 }
 
 
@@ -47,6 +52,23 @@ static NSString * const kUsernameTextFieldLabelText = @"Username";
     [self.usernameTextField mas_updateConstraints:^(MASConstraintMaker *make) {
         make.left.and.right.equalTo(self.view).with.insets(margin);
         make.bottom.equalTo(self.view.mas_centerY);
+    }];
+}
+
+
+- (void)buildPasswordTextField {
+    self.passwordTextField = [[MDCOutlinedTextField alloc] init];
+    [self.view addSubview:self.passwordTextField];
+    self.passwordTextField.label.text = kPasswordTextFieldLabelText;
+    self.passwordTextField.secureTextEntry = YES;
+}
+
+
+- (void)layoutPasswordTextField {
+    UIEdgeInsets margin = UIEdgeInsetsMake(0, kMargin, 0, kMargin);
+    [self.passwordTextField mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(self.view).with.insets(margin);
+        make.top.equalTo(self.usernameTextField.mas_bottom).with.offset(kYGutter);
     }];
 }
 
