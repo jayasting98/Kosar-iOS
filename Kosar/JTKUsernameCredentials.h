fcced1ffa7e935e7c9d58887589700a03fbe3b4e
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-24 14:41:35 +0800

    Add username-based credentials

---
+++ Kosar/JTKUsernameCredentials.h
@@ -0,0 +1,15 @@
+//
+//  JTKUsernameCredentials.h
+//  Kosar
+//
+//  Created by ByteDance on 24/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTKUsernameCredentials : NSObject
+
+@property (nonatomic) NSString *username;
+@property (nonatomic) NSString *password;
+
+@end
---
+++ Kosar/JTKUsernameCredentials.m
@@ -0,0 +1,16 @@
+//
+//  JTKUsernameCredentials.m
+//  Kosar
+//
+//  Created by ByteDance on 24/7/22.
+//
+
+#import "JTKUsernameCredentials.h"
+
+@interface JTKUsernameCredentials ()
+
+@end
+
+@implementation JTKUsernameCredentials
+
+@end
