a85b62e983d9b89c12cda29b3881d6766ceadd6e
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-26 00:33:01 +0800

    Add API method for creating a user

--- Kosar/JTKUsersService.h
+++ Kosar/JTKUsersService.h
@@ -7,8 +7,15 @@
 
 #import <Foundation/Foundation.h>
 
+@class JTKUser;
+
 @interface JTKUsersService : NSObject
 
 + (instancetype)sharedInstance;
+
+- (void)createUser:(JTKUser *)user
+    withClientErrorHandler:(void (^)(NSError *))clientErrorHandler
+    withServerErrorHandler:(void (^)(NSHTTPURLResponse *))serverErrorHandler
+        withSuccessHandler:(void (^)(JTKUser *))successHandler;
 
 @end
--- Kosar/JTKUsersService.m
+++ Kosar/JTKUsersService.m
@@ -7,7 +7,21 @@
 
 #import "JTKUsersService.h"
 
+#import "JTKApiService.h"
+#import "JTKUser.h"
+
 static NSString * const kBaseUsersPath = @"/users";
+
+static NSString * const kCreateUserRelativePath = @"/";
+
+static NSString * const kEmailAddressCreateUserRequestKey = @"emailAddress";
+static NSString * const kUsernameCreateUserRequestKey = @"username";
+static NSString * const kPasswordCreateUserRequestKey = @"password";
+
+static NSString * const kUserIdCreateUserResponseKey = @"userId";
+static NSString * const kEmailAddressCreateUserResponseKey = @"emailAddress";
+static NSString * const kUsernameCreateUserResponseKey = @"username";
+static NSString * const kDateTimeCreatedCreateUserResponseKey = @"dateTimeCreated";
 
 @interface JTKUsersService ()
 
@@ -29,6 +43,39 @@ static NSString * const kBaseUsersPath = @"/users";
     NSMutableString *completePath = [NSMutableString stringWithString:kBaseUsersPath];
     [completePath appendString:relativePath];
     return completePath;
+}
+
+
+- (void)createUser:(JTKUser *)user
+        withClientErrorHandler:(void (^)(NSError *))clientErrorHandler
+        withServerErrorHandler:(void (^)(NSHTTPURLResponse *))serverErrorHandler
+            withSuccessHandler:(void (^)(JTKUser *))successHandler {
+    NSDictionary *createUserRequestDictionary = @{
+        kEmailAddressCreateUserRequestKey: user.emailAddress,
+        kUsernameCreateUserRequestKey: user.username,
+        kPasswordCreateUserRequestKey: user.password,
+    };
+    NSData *createUserRequestData = [NSJSONSerialization dataWithJSONObject:createUserRequestDictionary
+                                                                    options:0
+                                                                      error:nil];
+    void (^successDataHandler)(NSData *) = ^(NSData *data){
+        if (!successHandler) {
+            return;
+        }
+        NSDictionary *responseDataDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        JTKUser *createdUser = [[JTKUser alloc] init];
+        createdUser.userId = responseDataDictionary[kUserIdCreateUserResponseKey];
+        createdUser.emailAddress = responseDataDictionary[kEmailAddressCreateUserResponseKey];
+        createdUser.username = responseDataDictionary[kUsernameCreateUserResponseKey];
+        createdUser.dateTimeCreated = responseDataDictionary[kDateTimeCreatedCreateUserResponseKey];
+        successHandler(createdUser);
+    };
+    NSString *completePath = [self createCompletePathWithRelativePath:kCreateUserRelativePath];
+    [[JTKApiService sharedInstance] postToPath:completePath
+                                      withBody:createUserRequestData
+                        withClientErrorHandler:clientErrorHandler
+                        withServerErrorHandler:serverErrorHandler
+                            withSuccessHandler:successDataHandler];
 }
 
 
