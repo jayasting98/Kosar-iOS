96f2f74e00420a469f163652c926fe6b6750126b
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-26 00:35:49 +0800

    Add sign up view-model

---
+++ Kosar/JTKSignUpViewModel.h
@@ -0,0 +1,12 @@
+//
+//  JTKSignUpViewModel.h
+//  Kosar
+//
+//  Created by ByteDance on 26/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTKSignUpViewModel : NSObject
+
+@end
---
+++ Kosar/JTKSignUpViewModel.m
@@ -0,0 +1,16 @@
+//
+//  JTKSignUpViewModel.m
+//  Kosar
+//
+//  Created by ByteDance on 26/7/22.
+//
+
+#import "JTKSignUpViewModel.h"
+
+@interface JTKSignUpViewModel ()
+
+@end
+
+@implementation JTKSignUpViewModel
+
+@end
