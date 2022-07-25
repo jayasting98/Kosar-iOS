901384e9062fe8319b2c61cb262f613051922649
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-25 22:32:43 +0800

    Add authentication modal view controller

---
+++ Kosar/JTKAuthenticationModalController.h
@@ -0,0 +1,12 @@
+//
+//  JTKAuthenticationModalController.h
+//  Kosar
+//
+//  Created by ByteDance on 25/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTKAuthenticationModalController : UINavigationController
+
+@end
---
+++ Kosar/JTKAuthenticationModalController.m
@@ -0,0 +1,20 @@
+//
+//  JTKAuthenticationModalController.m
+//  Kosar
+//
+//  Created by ByteDance on 25/7/22.
+//
+
+#import "JTKAuthenticationModalController.h"
+
+@interface JTKAuthenticationModalController ()
+
+@end
+
+@implementation JTKAuthenticationModalController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
