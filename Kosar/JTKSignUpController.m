f7752b5112455f8c12b347eb03b7ed26e0279a3a
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-25 23:03:24 +0800

    Add username text field in sign up controller

--- Kosar/JTKSignUpController.m
+++ Kosar/JTKSignUpController.m
@@ -7,7 +7,16 @@
 
 #import "JTKSignUpController.h"
 
+#import "Masonry.h"
+#import "MaterialTextControls+OutlinedTextFields.h"
+
+static CGFloat const kMargin = 16;
+
+static NSString * const kUsernameTextFieldLabelText = @"Username";
+
 @interface JTKSignUpController ()
+
+@property (nonatomic) MDCOutlinedTextField *usernameTextField;
 
 @end
 
@@ -16,6 +25,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.whiteColor;
+    [self buildUsernameTextField];
+}
+
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    [self layoutUsernameTextField];
+}
+
+
+- (void)buildUsernameTextField {
+    self.usernameTextField = [[MDCOutlinedTextField alloc] init];
+    [self.view addSubview:self.usernameTextField];
+    self.usernameTextField.label.text = kUsernameTextFieldLabelText;
+}
+
+
+- (void)layoutUsernameTextField {
+    UIEdgeInsets margin = UIEdgeInsetsMake(0, kMargin, 0, kMargin);
+    [self.usernameTextField mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(self.view).with.insets(margin);
+        make.bottom.equalTo(self.view.mas_centerY);
+    }];
 }
 
 
